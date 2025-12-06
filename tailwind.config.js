500e59d3d2f30d2e16b6646d21fb9eba66bfbcc2
Aneesh <aneeshreddy.ar@gmail.com>
2025-12-06 11:25:17 +0530

    Sending some changes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,tsx}",
+    "./src/pages/**/*.{js,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
